0b47adfb9021b2337795ea6bc9f38f69529037c2
包子 <baozi65535@163.com>
2020-11-05 08:58:36 +0800

    第一次更新

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
-    <h1>黑马头条内容发布系统</h1>
 <!--    路由出口-->
     <router-view/>
   </div>
 </template>
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      num: 6
+    }
+  }
 }
 </script>
-<style  lang="less">
-
-</style>
+<style  lang="less"></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '@/views/login'
+import Home from '@/views/home'
+import Layout from '@/views/layout'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/',
+    component: Layout,
+    children: [
+      {
+        path: '', // path为空，会作为默认子路由渲染
+        name: 'home',
+        component: Home
+      }
+    ]
+  }
+]
 
 const router = new VueRouter({
   routes
